58b5c5846f3138790b94c2ec370521068db96791
HelgaAthame <athamethedarkest@gmail.com>
2024-11-19 14:10:28 +0300

    feat: add rewrites

--- next.config.mjs
+++ next.config.mjs
@@ -5,20 +5,28 @@
 
 /** @type {import("next").NextConfig} */
 const config = {
-  reactStrictMode: true,
-  output: 'standalone',
-  // basePath: "/store-example",
+  reactStrictMode: true, // Рекомендуется оставить включенным
+  output: "standalone", // Для Docker-окружений
   images: {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "fakestoreapi.com",
+        hostname: "fakestoreapi.com", // Разрешенные внешние домены для изображений
       },
       {
         protocol: "https",
-        hostname: "i.imgur.com",
+        hostname: "i.imgur.com", // Еще один разрешенный внешний домен
       },
     ],
+  },
+  // Дополнительно можно использовать rewrites для проксирования API-запросов
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*", // Локальный путь
+        destination: "https://fakestoreapi.com/:path*", // Проксирование внешнего API
+      },
+    ];
   },
 };
 
