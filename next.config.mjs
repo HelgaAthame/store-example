4a16c10be7e6fb84f9f0a6658286ede190aa9bb8
HelgaAthame <athamethedarkest@gmail.com>
2024-11-19 14:05:09 +0300

    feat: change nextconfig

--- next.config.mjs
+++ next.config.mjs
@@ -6,7 +6,7 @@
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
-  // output: "export",
+  output: 'standalone',
   // basePath: "/store-example",
   images: {
     remotePatterns: [
